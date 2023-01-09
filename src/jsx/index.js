4da1280e727e526573af9ef07c66c9b42c7d0cd7
Joe Howard <joe.howard.au@icloud.com>
2023-01-09 15:09:22 +1100

    Commented code

--- src/jsx/index.js
+++ src/jsx/index.js
@@ -1,6 +1,12 @@
 import parse, { COMPONENT_CACHE } from './parser';
 import { is_object, is_undefined, callable_name } from '../utils/index';
 
+/**
+ * Mian API to convert JSX from string into 'createElement'
+ *
+ * @param  {string}  jsx   JSX in string format.
+ * @param  {object}  vars  Key/value pairs of dependencies
+ */
 export function jsx(jsx, vars)
 {
     if (!is_undefined(vars) && !is_object(vars))
@@ -11,6 +17,12 @@ export function jsx(jsx, vars)
     return parse(jsx, vars);
 }
 
+/**
+ * Register a dependencies
+ *
+ * @param  {mixed}   component   Can be any variable.
+ * @param  {string}  key         Name to set dependency as
+ */
 export function register(component, key)
 {
     key = is_undefined(key) ? callable_name(component) : key;
